469d12c25bcba34647b455ef3834e4006017d329
Yatharth Jain <yatharthjain@Yatharths-MacBook-Pro.local>
2023-01-06 14:27:34 +0000

    first commit

---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
+import './header.css';
+
+const Header = () => (
+  <div className="gpt3__header section__padding" id="home">
+    <div className="gpt3__header-content">
+      <h1 className="gradient__text">Let&apos;s Build Something amazing with OpenAI</h1>
+      <p>However, it is unpleasant to indulge in bed for journey help. Not all ideas include blessing. Everything changes for the better when there is indulgence. Party we years order ask for allowed to.</p>
+
+      <div className="gpt3__header-content__input">
+        <input type="email" placeholder="Your Email Address" />
+        <button type="button">Get Started</button>
+      </div>
+
+      <div className="gpt3__header-content__people">
+        <img src={people} />
+        <p>1,600 requested access a visit in last 24 hours</p>
+      </div>
+    </div>
+
+    <div className="gpt3__header-image">
+      <img src={ai} />
+    </div>
+  </div>
+);
+
+export default Header;
